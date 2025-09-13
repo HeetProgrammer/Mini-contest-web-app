bae87f19bcc35e0078ab549e7791233d2a19a753
Heet <aofavalon@gmail.com>
2025-09-13 18:31:54 +0530

    Finished user registration and login features. Now working on main functionality

---
+++ directory/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database_contest.db'
+# 24 character Secret Key for Protection. To generate a key, run secrets.token_hex()
+app.config['SECRET_KEY'] = '5e79781d9cfad8ff4a207156'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = 'info'
+
+import migrate
+
+
+from directory import routes
+from directory import models
